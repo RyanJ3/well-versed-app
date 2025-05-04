41fc44a05e0ca28658eedf9e9b62c4db379ab9c8
rmcelhaney <ryanmcelhaney@mac.com>
2025-05-04 18:26:03 -0400

    substatial rework

--- frontend/src/app/bible-tracker/bible-tracker.component.ts
+++ frontend/src/app/bible-tracker/bible-tracker.component.ts
@@ -1,28 +1,101 @@
 // bible-tracker.component.ts - Main component for the Bible Memorization Tracker
-import { Component } from '@angular/core';
-import { BibleStatsComponent } from './bible-stats.component';
-import { BookSelectorComponent } from './components/book-selector/book-selector.component';
-import { ChapterProgressComponent } from './components/chapter-progress/chapter-progress.component';
-import { ChapterSelectorComponent } from './components/chapter-selector/chapter-selector.component';
-import { GroupSelectorComponent } from './components/group-selector/group-selector.component';
-import { TestamentSelectorComponent } from './components/testament-selector/testament-selector.component';
+import { Component, Input } from '@angular/core';
+import { BibleBook, BibleChapter, BibleData, BibleGroup, BibleTestament } from "../models/bible.model";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bible-tracker',
   templateUrl: './bible-tracker.component.html',
-  imports: [
-    TestamentSelectorComponent,
-    GroupSelectorComponent,
-    BookSelectorComponent,
-    ChapterSelectorComponent,
-    ChapterProgressComponent,
+  imports: [CommonModule],
+  styleUrls: [
+    './style-sheets/book-info.scss',
+    './style-sheets/book-selector.scss',
+    './style-sheets/chapter-progress.scss',
+    './style-sheets/chapter-selector.scss',
+    './style-sheets/group-selector.scss',
+    './style-sheets/testament-selector.scss',
+    './style-sheets/verse-selector.scss',
   ],
-  styleUrls: ['./bible-tracker.component.css'],
 })
-export class BibleTrackerComponent extends BibleStatsComponent{
+export class BibleTrackerComponent {
 
-  constructor() {
-    super();
+  private bibleData: BibleData = new BibleData();
+
+  selectedTestament: BibleTestament = this.defaultTestament;
+  selectedGroup: BibleGroup = this.defaultGroup;
+  selectedBook: BibleBook = this.defaultBook;
+  selectedChapter: BibleChapter = this.defaultChapter;
+
+  constructor() { }
+
+  setTestament(testament: BibleTestament): void {
+    this.selectedTestament = testament;
   }
+
+  setGroup(group: BibleGroup): void {
+    this.selectedGroup = group;
+  }
+
+  setBook(book: BibleBook): void {
+    this.selectedBook = book;
+  }
+
+  setChapter(chapter: BibleChapter): void {
+    this.selectedChapter = chapter;
+  }
+
+  // Convenience methods for accessing Bible data
+  getTestamentByName(testamentName: string): BibleTestament {
+    return this.bibleData.getTestamentByName(testamentName);
+  }
+
+  getGroupByName(groupName: string): BibleBook[] {
+    return this.bibleData.getGroupByName(groupName).books;
+  }
+
+  getBookByName(bookName: string): BibleBook {
+    return this.bibleData.getBookByName(bookName);
+  }
+
+  get oldTestament(): BibleTestament {
+    return this.getTestamentByName('OLD');
+  }
+
+  get newTestament(): BibleTestament {
+    return this.getTestamentByName('NEW');
+  }
+
+  get defaultTestament(): BibleTestament {
+    return this.oldTestament;
+  }
+
+  get testaments(): BibleTestament[] {
+    return [this.oldTestament, this.newTestament];
+  }
+
+  get percentComplete(): number {
+    return this.bibleData.percentComplete;
+  }
+
+  get defaultGroup(): BibleGroup {
+    return this.defaultBook.group;
+  }
+
+  get defaultBook(): BibleBook {
+    return this.getBookByName("Psalms");
+  }
+
+  get defaultChapter(): BibleChapter {
+    return this.getBookByName("Psalms").chapters[22];
+  }
+
+  selectAllVerses() {
+    this.selectedChapter.verses.forEach(verse => verse.memorized = true);
+  }
+
+  clearAllVerses() {
+    this.selectedChapter.verses.forEach(verse => verse.memorized = false);
+  }
+
 
 }
--- frontend/src/app/bible-tracker/bible-tracker.module.ts
+++ frontend/src/app/bible-tracker/bible-tracker.module.ts
@@ -1,33 +1,12 @@
 // bible-tracker.module.ts
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { GroupSelectorComponent } from './components/group-selector/group-selector.component';
-import { BookSelectorComponent } from './components/book-selector/book-selector.component';
-import { BookInfoComponent } from './components/book-info/book-info.component';
-import { ChapterSelectorComponent } from './components/chapter-selector/chapter-selector.component';
-import { ChapterProgressComponent } from './components/chapter-progress/chapter-progress.component';
-import { TestamentSelectorComponent } from './components/testament-selector/testament-selector.component';
-import { VerseSelectorComponent } from './components/verse-selector/verse-selector.component';
 import { BibleTrackerComponent } from './bible-tracker.component';
-import { ConfirmationModalComponent } from '../shared/components/notification/confirmation-modal';
 
 @NgModule({
   declarations: [
-    // No declarations as all components are standalone
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    TestamentSelectorComponent,
-    GroupSelectorComponent,
-    BookSelectorComponent,
-    VerseSelectorComponent,
-    BibleTrackerComponent,
-    BookInfoComponent,
-    ChapterProgressComponent,
-    ChapterSelectorComponent,
-    ConfirmationModalComponent,
+    BibleTrackerComponent
   ],
   exports: [BibleTrackerComponent],
 })
