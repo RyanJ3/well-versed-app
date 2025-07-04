95b6d98ac210e1cf853feaf3389e607f4ea728eb
RyanJ3 <33846691+RyanJ3@users.noreply.github.com>
2025-07-03 21:30:02 -0400

    add arcgis typings to avoid compile errors

---
+++ frontend/src/arcgis-typings.d.ts
@@ -0,0 +1,8 @@
+declare module '@arcgis/core/Map';
+declare module '@arcgis/core/views/MapView';
+declare module '@arcgis/core/Graphic';
+declare module '@arcgis/core/layers/GraphicsLayer';
+declare module '@arcgis/core/symbols/SimpleMarkerSymbol';
+declare module '@arcgis/core/symbols/TextSymbol';
+declare module '@arcgis/core/geometry/Point';
+declare module '@arcgis/core/PopupTemplate';
